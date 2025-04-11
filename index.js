1fbcb6a90696329f9c37bd2288257d5715be0153
asherkim815 <asherkim815@gmail.com>
2025-04-12 00:24:08 +0900

    organize and refine css

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-document.querySelectorAll('.sign-up-button').forEach((button, index) => {
+document.querySelectorAll('.sign-up').forEach((button, index) => {
   button.addEventListener('mouseover', function () {
-    this.classList.add('gradient-background1');
+    this.classList.add('gradient-background');
     document
-      .querySelector(`.pricing-card-head${index + 1}`)
-      .classList.add('gradient-background1');
+      .querySelector(`.card-head${index + 1}`)
+      .classList.add('gradient-background');
   });
 });
 
-document.querySelectorAll('.sign-up-button').forEach((button, index) => {
+document.querySelectorAll('.sign-up').forEach((button, index) => {
   button.addEventListener('mouseout', function () {
-    this.classList.remove('gradient-background1');
+    this.classList.remove('gradient-background');
     document
-      .querySelector(`.pricing-card-head${index + 1}`)
-      .classList.remove('gradient-background1');
+      .querySelector(`.card-head${index + 1}`)
+      .classList.remove('gradient-background');
   });
 });
